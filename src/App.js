07195e40e817bd7670e83e42f1c21723dfd22f38
RamolaShishupal <ramoshishupal@gmail.com>
2021-01-02 23:20:12 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
+import Header from "./Header";
 import "./style.css";
 
 export default function App() {
   return (
     <div>
       <h1>lets make amazone 🚀</h1>
+      <Header/>
    
     </div>
  
 
   );
-   {/*header*/}
+    
     {/*home*/}
 }
--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,32 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className='header'>
-     
+    <img 
+    className='header_logo'
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGxzYaLsYlO1-WTsPVzGNDG5qico37U5X1GA&usqp=CAU"/>
+
+    <div className='header__search'>
+    <input className='header__searchInput' type='Text'/>
+    {/*Logo*/}
+    
+    </div>
+     <div className='header__nav'>
+
+     <div className='header__option'>
+     <span className='header__optionLine1'>Hello guest</span>
+     <span className='header__optionLine2'>Sign In</span>
+     </div>
+
+     <div className='header__option'>
+      <span className='header__optionLine1'>Returns</span>
+     <span className='header__optionLine2'>Order</span>
+
+     </div>
+
+     <div className='header__option'></div>
+      <span className='header__optionLine1'>Your</span>
+     <span className='header__optionLine2'>Prime</span>
+     </div>
    
     </div>
  

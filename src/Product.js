59401d659e3e7406fcd8530d36b182520cbf5c89
RamolaShishupal <ramoshishupal@gmail.com>
2021-01-04 20:37:20 +0530

    1 file modified, 1 added

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import "./style.css";
+import "./Product.css";
+
 
 export default function Product() {
   return (
-    <div>
+    <div className='product__container' >
      
    
     </div>

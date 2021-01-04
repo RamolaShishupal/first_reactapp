99a7a79945cd7f40334f1142f6740f0fa56cd9b0
RamolaShishupal <ramoshishupal@gmail.com>
2021-01-04 23:29:32 +0530

    2 files modified

--- src/Product.js
+++ src/Product.js
@@ -8,9 +8,15 @@ export default function Product() {
     <div className='product' >
     <div className='product_info'>
     <p>yo yo </p>
-    <p className='product_proce'>
+    <p className='product_price'>
     <small>$</small>
-    <strong>19.99</strong></p>
+    <strong>19.99</strong>
+    </p>
+
+    <div className='product_rating'>
+    <p>⭐⭐⭐⭐⭐</p>
+
+    </div>
     </div>
      
    

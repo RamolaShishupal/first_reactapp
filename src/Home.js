50ec649ac30c1e52124f37e866341ad909fdbad6
RamolaShishupal <ramoshishupal@gmail.com>
2021-01-04 14:46:53 +0530

    2 files modified

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,11 @@ import "./home.css";
 export default function Home() {
   return (
     <div className='home'>
-    <p>home </p>
+    <div className='home__container'>
+    <img 
+    className='home__img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzE4jjXjORx1z9rzGoJiW9pRmKlW5xeuVCsQ&usqp=CAU' alt='error' />
+    </div>
+   
 
      
     

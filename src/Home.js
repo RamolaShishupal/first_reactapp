4b33f4c8c04c586f69eb6871e0b346e280bcff6c
RamolaShishupal <ramoshishupal@gmail.com>
2021-01-04 20:19:33 +0530

    1 file modified

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,23 @@ export default function Home() {
     <div className='home__container'>
     <img 
     className='home__img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA4V9Gl4KPBuZMkpje_qs32Y2qdpl5UPK3AQ&usqp=CAU' alt='error' />
+    <div className='home__row'>
+    {/*product */}
+     {/*product */}
     </div>
-   
 
-     
+    <div className='home__row'>
+    {/*product */}
+     {/*product */}
+      {/*product */}
+    </div>
+
+    <div className='home__row'>
+    {/*product */}
     
-   
+    </div>
+
+    </div>
     </div>
  
 
